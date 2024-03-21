6b2d3e1bc4ae4e31f9b1239dfdebb8cd5d1fd8ed
mdorka22 <mdorka22@gmail.com>
2024-03-21 10:32:04 +0100

    házi

--- HajósTeszt/Form1.cs
+++ HajósTeszt/Form1.cs
@@ -58,8 +58,8 @@ namespace HajósTeszt
             textBox3.Text = kérdés.Válasz3;
 
             textBox1.BackColor = Color.White;
-            textBox2.BackColor=Color.White;
-            textBox3.BackColor=Color.White;
+            textBox2.BackColor = Color.White;
+            textBox3.BackColor = Color.White;
 
             if (string.IsNullOrEmpty(kérdés.URL))
             {
@@ -84,12 +84,15 @@ namespace HajósTeszt
         {
             textBox1.BackColor = Color.Salmon;
             Színezés();
+            Számláló();
         }
 
         private void textBox2_Click(object sender, EventArgs e)
         {
             textBox2.BackColor = Color.Salmon;
             Színezés();
+            Számláló();
+
 
         }
 
@@ -97,12 +100,42 @@ namespace HajósTeszt
         {
             textBox3.BackColor = Color.Salmon;
             Színezés();
+            Számláló();
+
         }
         void Színezés()
-        { int helyesVálasz = AktuálisKérdések[MegjelenítettKérdésSzáma].HelyesVálasz;
-            if (helyesVálasz==1) textBox1.BackColor= Color.Green;
+        {
+            int helyesVálasz = AktuálisKérdések[MegjelenítettKérdésSzáma].HelyesVálasz;
+            if (helyesVálasz == 1) textBox1.BackColor = Color.Green;
             if (helyesVálasz == 2) textBox2.BackColor = Color.Green;
             if (helyesVálasz == 3) textBox3.BackColor = Color.Green;
+        }
+        void Számláló()
+        {
+
+
+            int helyesVálasz = AktuálisKérdések[MegjelenítettKérdésSzáma].HelyesVálasz;
+            int helyesVálaszokSzáma = AktuálisKérdések[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma;
+            if (helyesVálasz == 1) helyesVálaszokSzáma++;
+            if (helyesVálasz == 2) helyesVálaszokSzáma++;
+            if (helyesVálasz == 3) helyesVálaszokSzáma++;
+            AktuálisKérdések[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma = helyesVálaszokSzáma;
+
+            
+
+            if (helyesVálaszokSzáma== 3)
+            {
+                // Cseréljük ki a kérdést az összes kérdést tartalmazó lista első (nulladik) elemére
+                AktuálisKérdések[MegjelenítettKérdésSzáma] = ÖsszesKérdés[0];
+
+                // Vegyük ki az összes kérdést tartalmazó listából a nulladik elemet
+                ÖsszesKérdés.RemoveAt(0);
+            }
+
+            dataGridView1.Refresh();
+
+
+
         }
     }
 }
